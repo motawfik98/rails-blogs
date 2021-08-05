9d45b432ab1e69ccdf9414ffaeb181f7efa71735
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-05 13:43:57 +0200

    add total count class method to the module

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -6,6 +6,12 @@ module Visible
     validates :status, inclusion: {in: VALID_STATUS}
   end
 
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
   def archived?
     status == 'archived'
   end
