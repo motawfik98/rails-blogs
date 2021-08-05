165d026af69013991105cd47c989214b556aeeaf
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-05 13:55:36 +0200

    add destroy controller for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,16 @@ class CommentsController < ApplicationController
     render json: comment
   end
 
+  def destroy
+    article = Article.find(params[:article_id])
+    comment = article.comments.find(params[:id])
+    comment.destroy
+    render json: {
+      status: 200,
+      message: "Success"
+    }
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
