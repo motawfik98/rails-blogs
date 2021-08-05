c412fa6c7a6d50b85a9e58280fcd5491de7f017d
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-05 03:26:03 +0200

    use resources instead of listing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles # generate CRUD routes for articles (eg. GET /articles GET /articles/:id POST /articles, etc...)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
