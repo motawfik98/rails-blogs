fd4f8199607c15fa767d0dfaa11675f3b5cdf913
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-05 13:32:34 +0200

    add create comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+    article = Article.find(params[:article_id])
+    comment = article.comments.create(comment_params)
+    render json: comment
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles # generate CRUD routes for articles (eg. GET /articles GET /articles/:id POST /articles, etc...)
+  # generate CRUD routes for articles (eg. GET /articles GET /articles/:id POST /articles, etc...)
+  resources :articles do
+    resources :comments # nesting comments under articles
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
