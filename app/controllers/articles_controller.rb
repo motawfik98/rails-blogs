ed5ffcb381df559da686eeae50ed923561feb515
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-05 03:34:24 +0200

    add create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,21 @@ class ArticlesController < ApplicationController
     article = Article.find(params[:id])
     render json: article
   end
+
+  def create
+    article = Article.new(article_params)
+    if article.save
+      render json: article
+    else
+      render json: {
+        message: "bad request, invalid format",
+        status: 400
+      }
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
